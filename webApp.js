bcbdf42f47efdb1e117c551012a702164c038104
Gregor <jakob.srumpf@gmail.com>
2021-11-27 12:08:02 +0100

    recimo da dela mp

--- webApp.js
+++ webApp.js
@@ -3,7 +3,9 @@ let joinBtn, aiBtn, createBtn;
 var turn = null; // 0 this player, 1 remote
 var player_icon = 'o';
 var conn;
-var board_array = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+var board_array = ['-', '-', '-',
+ '-', '-', '-'
+ , '-', '-', '-']
 
 window.onload = () => {
     joinBtn = document.getElementById('joinButton')
@@ -17,6 +19,44 @@ function emit(type, data){
             type: type,
             data: data
         })
+    }
+}
+
+function checkBoard(){
+    let win = 0
+    if(board_array[0] == board_array[3] && board_array[3] == board_array[6] && board_array[6] != '-')win = 1
+    else if(board_array[1] == board_array[4] && board_array[4] == board_array[7] && board_array[7] != '-')win = 2
+    else if(board_array[2] == board_array[5] && board_array[5] == board_array[8] && board_array[8] != '-')win = 3
+
+    else if(board_array[0] == board_array[1] && board_array[1] == board_array[2] && board_array[2] != '-')win = 4
+    else if(board_array[3] == board_array[4] && board_array[4] == board_array[5] && board_array[5] != '-')win = 5
+    else if(board_array[6] == board_array[7] && board_array[7] == board_array[8] && board_array[8] != '-')win = 6
+
+    else if(board_array[0] == board_array[4] && board_array[4] == board_array[8] && board_array[8] != '-')win = 7
+    else if(board_array[2] == board_array[4] && board_array[4] == board_array[6] && board_array[6] != '-')win = 8
+
+    if(win != 0){
+        let winner = '-'
+        switch(win){
+            case 1: winner = board_array[0]; break;
+            case 2: winner = board_array[1]; break;
+            case 3: winner = board_array[2]; break;
+
+            case 4: winner = board_array[0]; break;
+            case 5: winner = board_array[3]; break;
+            case 6: winner = board_array[6]; break;
+
+            case 7: winner = board_array[0]; break;
+            case 8: winner = board_array[2]; break;
+            default: return;
+        }
+        turn = 1
+        if(winner == player_icon){
+            alert('Yay, ur ass won')
+        }
+        else{
+            alert('Vec srece prihodnjic, krompircek')
+        }
     }
 }
 
@@ -34,6 +74,7 @@ function move(x){
 
 function setState(block, state){
     board_array[block] = state
+    checkBoard()
     let image = document.getElementById(block)
     let imgPath;
     switch(state){
